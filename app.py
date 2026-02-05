5bf730f478a07fd5b178f3ea1119e76e38ac10bc
shritejkawadkar <shritejkawadkar14@gmail.com>
2026-02-05 10:54:03 +0000

    cm1

---
+++ app.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print("Addition:", add(5, 3))
+    print("Subtraction:", sub(10, 4))
+
---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import add, sub
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_sub():
+    assert sub(5, 2) == 3
+
